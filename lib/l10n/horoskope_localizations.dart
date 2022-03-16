37f275e1e671d73bf10a15d3b4e5c5132b9124a4
Alexandr Ilyin <61319502+ilyin0@users.noreply.github.com>
2022-03-16 12:55:30 +0300

    feat: init localizations (#2)

---
+++ lib/l10n/horoskope_localizations.dart
@@ -0,0 +1,137 @@
+
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart' as intl;
+
+import 'horoskope_localizations_en.dart';
+
+/// Callers can lookup localized strings with an instance of HoroskopeLocalizations returned
+/// by `HoroskopeLocalizations.of(context)`.
+///
+/// Applications need to include `HoroskopeLocalizations.delegate()` in their app's
+/// localizationDelegates list, and the locales they support in the app's
+/// supportedLocales list. For example:
+///
+/// ```
+/// import 'l10n/horoskope_localizations.dart';
+///
+/// return MaterialApp(
+///   localizationsDelegates: HoroskopeLocalizations.localizationsDelegates,
+///   supportedLocales: HoroskopeLocalizations.supportedLocales,
+///   home: MyApplicationHome(),
+/// );
+/// ```
+///
+/// ## Update pubspec.yaml
+///
+/// Please make sure to update your pubspec.yaml to include the following
+/// packages:
+///
+/// ```
+/// dependencies:
+///   # Internationalization support.
+///   flutter_localizations:
+///     sdk: flutter
+///   intl: any # Use the pinned version from flutter_localizations
+///
+///   # rest of dependencies
+/// ```
+///
+/// ## iOS Applications
+///
+/// iOS applications define key application metadata, including supported
+/// locales, in an Info.plist file that is built into the application bundle.
+/// To configure the locales supported by your app, you’ll need to edit this
+/// file.
+///
+/// First, open your project’s ios/Runner.xcworkspace Xcode workspace file.
+/// Then, in the Project Navigator, open the Info.plist file under the Runner
+/// project’s Runner folder.
+///
+/// Next, select the Information Property List item, select Add Item from the
+/// Editor menu, then select Localizations from the pop-up menu.
+///
+/// Select and expand the newly-created Localizations item then, for each
+/// locale your application supports, add a new item and select the locale
+/// you wish to add from the pop-up menu in the Value field. This list should
+/// be consistent with the languages listed in the HoroskopeLocalizations.supportedLocales
+/// property.
+abstract class HoroskopeLocalizations {
+  HoroskopeLocalizations(String locale) : localeName = intl.Intl.canonicalizedLocale(locale.toString());
+
+  final String localeName;
+
+  static HoroskopeLocalizations? of(BuildContext context) {
+    return Localizations.of<HoroskopeLocalizations>(context, HoroskopeLocalizations);
+  }
+
+  static const LocalizationsDelegate<HoroskopeLocalizations> delegate = _HoroskopeLocalizationsDelegate();
+
+  /// A list of this localizations delegate along with the default localizations
+  /// delegates.
+  ///
+  /// Returns a list of localizations delegates containing this delegate along with
+  /// GlobalMaterialLocalizations.delegate, GlobalCupertinoLocalizations.delegate,
+  /// and GlobalWidgetsLocalizations.delegate.
+  ///
+  /// Additional delegates can be added by appending to this list in
+  /// MaterialApp. This list does not have to be used at all if a custom list
+  /// of delegates is preferred or required.
+  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates = <LocalizationsDelegate<dynamic>>[
+    delegate,
+    GlobalMaterialLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+  ];
+
+  /// A list of this localizations delegate's supported locales.
+  static const List<Locale> supportedLocales = <Locale>[
+    Locale('en')
+  ];
+
+  /// No description provided for @horoskope.
+  ///
+  /// In en, this message translates to:
+  /// **'Horoskope'**
+  String get horoskope;
+
+  /// No description provided for @homePage.
+  ///
+  /// In en, this message translates to:
+  /// **'Home Page'**
+  String get homePage;
+}
+
+class _HoroskopeLocalizationsDelegate extends LocalizationsDelegate<HoroskopeLocalizations> {
+  const _HoroskopeLocalizationsDelegate();
+
+  @override
+  Future<HoroskopeLocalizations> load(Locale locale) {
+    return SynchronousFuture<HoroskopeLocalizations>(lookupHoroskopeLocalizations(locale));
+  }
+
+  @override
+  bool isSupported(Locale locale) => <String>['en'].contains(locale.languageCode);
+
+  @override
+  bool shouldReload(_HoroskopeLocalizationsDelegate old) => false;
+}
+
+HoroskopeLocalizations lookupHoroskopeLocalizations(Locale locale) {
+
+
+  // Lookup logic when only language code is specified.
+  switch (locale.languageCode) {
+    case 'en': return HoroskopeLocalizationsEn();
+  }
+
+  throw FlutterError(
+    'HoroskopeLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
+    'an issue with the localizations generation tool. Please file an issue '
+    'on GitHub with a reproducible sample app and the gen-l10n configuration '
+    'that was used.'
+  );
+}
