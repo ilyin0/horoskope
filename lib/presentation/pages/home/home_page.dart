32c30fafe4c10e7145b93f58f506116cd15a5bcd
Alexandr Ilyin <61319502+ilyin0@users.noreply.github.com>
2022-03-20 19:12:20 +0300

    feat: add info card widget (#6)

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -5,6 +5,7 @@ import 'package:horoskope/presentation/pages/home/home_cubit.dart';
 import 'package:horoskope/presentation/pages/home/home_state.dart';
 import 'package:horoskope/presentation/themes/horoskope_theme.dart';
 import 'package:horoskope/presentation/utils/build_context_ext.dart';
+import 'package:horoskope/presentation/widgets/info_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,11 +20,11 @@ class HomePage extends StatelessWidget {
             title: Text(context.localizations.horoskope),
           ),
           body: Center(
-            child: Text(
-              state.todayForecast,
-              style: TextStyle(
-                color: HoroskopeTheme.color(context).primaryBlue,
-              ),
+            child: InfoCard(
+              title: 'Title of the Card',
+              body: state.todayForecast,
+              textTheme: HoroskopeTheme.text(context),
+              width: 300,
             ),
           ),
         );
---
+++ lib/presentation/utils/default_values.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const infoCardShadowColor = Color(0xFF3379AF);
+const infoCardColor = Color(0xFFF7F4F3);
