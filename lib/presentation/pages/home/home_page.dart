33cbadbfaf73d5ec7a2fcdfe436a3de640416047
Alexandr Ilyin <61319502+ilyin0@users.noreply.github.com>
2022-03-22 14:00:03 +0300

    develop: add additional widget (ElevatedCard) (#8)

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -6,6 +6,7 @@ import 'package:horoskope/presentation/pages/home/home_state.dart';
 import 'package:horoskope/presentation/themes/horoskope_theme.dart';
 import 'package:horoskope/presentation/utils/build_context_ext.dart';
 import 'package:horoskope/presentation/widgets/horoskope_button.dart';
+import 'package:horoskope/presentation/widgets/info_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,34 +20,47 @@ class HomePage extends StatelessWidget {
           appBar: AppBar(
             title: Text(context.localizations.horoskope),
           ),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+          body: Stack(
             children: [
-              const SizedBox(height: 8, width: double.infinity),
-              HoroskopeButton(
-                child: const Text('Sign Up'),
-                onTap: () {},
-                style: HoroskopeTheme.button(context).primary,
+              const Placeholder(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 8, width: double.infinity),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: InfoCard(
+                      body: state.todayForecast,
+                      textTheme: HoroskopeTheme.text(context),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  HoroskopeButton(
+                    child: const Text('Sign Up'),
+                    onTap: () {},
+                    style: HoroskopeTheme.button(context).primary,
+                  ),
+                  const SizedBox(height: 8),
+                  HoroskopeButton(
+                    child: const Text('Sign Up'),
+                    onTap: () {},
+                    style: HoroskopeTheme.button(context).secondary1,
+                  ),
+                  const SizedBox(height: 8),
+                  HoroskopeButton(
+                    child: const Text('Sign Up'),
+                    onTap: () {},
+                    style: HoroskopeTheme.button(context).secondary2,
+                  ),
+                  const SizedBox(height: 8),
+                  HoroskopeButton(
+                    child: const Text('Sign Up'),
+                    onTap: () {},
+                    style: HoroskopeTheme.button(context).disabled,
+                  ),
+                  const SizedBox(height: 8),
+                ],
               ),
-              const SizedBox(height: 8),
-              HoroskopeButton(
-                child: const Text('Sign Up'),
-                onTap: () {},
-                style: HoroskopeTheme.button(context).secondary1,
-              ),
-              const SizedBox(height: 8),
-              HoroskopeButton(
-                child: const Text('Sign Up'),
-                onTap: () {},
-                style: HoroskopeTheme.button(context).secondary2,
-              ),
-              const SizedBox(height: 8),
-              HoroskopeButton(
-                child: const Text('Sign Up'),
-                onTap: () {},
-                style: HoroskopeTheme.button(context).disabled,
-              ),
-              const SizedBox(height: 8),
             ],
           ),
         );
