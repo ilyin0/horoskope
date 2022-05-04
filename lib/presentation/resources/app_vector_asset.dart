d5db0d755abab2417b8d44032b671ed5a78a3830
ilyin0 <ilyindeveloper@gmail.com>
2022-05-04 14:53:26 +0300

    feat: add Zodiac signs icons

--- lib/presentation/resources/app_vector_asset.dart
+++ lib/presentation/resources/app_vector_asset.dart
@@ -4,4 +4,18 @@ abstract class AppVectorAsset {
   static const heartIconBorder = 'assets/icons/heart_icon_border.svg';
   static const heartIconFilled = 'assets/icons/heart_icon_filled.svg';
   static const userIcon = 'assets/icons/user_icon.svg';
+
+  // Zodiac signs
+  static const aquarius = 'assets/icons/aquarius.svg';
+  static const aries = 'assets/icons/aries.svg';
+  static const cancer = 'assets/icons/cancer.svg';
+  static const capricorn = 'assets/icons/capricorn.svg';
+  static const gemini = 'asseta/icons/gemini.svg';
+  static const leo = 'asseta/icons/leo.svg';
+  static const libra = 'asseta/icons/libra.svg';
+  static const pisces = 'asseta/icons/pisces.svg';
+  static const sagittarius = 'asseta/icons/sagittarius.svg';
+  static const scorpio = 'asseta/icons/scorpio.svg';
+  static const taurus = 'asseta/icons/taurus.svg';
+  static const virgo = 'asseta/icons/virgo.svg';
 }
